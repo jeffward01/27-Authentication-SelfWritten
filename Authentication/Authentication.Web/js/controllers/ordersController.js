72dac8a350329475bbcc3c447e089dce56a0d80c
jeffward01 <jeffwardcoding@gmail.com>
2015-11-03 15:08:07 -0800

    controllers building out

--- Authentication/Authentication.Web/js/controllers/ordersController.js
+++ Authentication/Authentication.Web/js/controllers/ordersController.js
@@ -1,1 +1,18 @@
-﻿
+﻿'use strict';
+app.factory('ordersService', ['$http', function ($http) {
+
+    var serviceBase = 'http://ngauthenticationapi.azurewebsites.net/';
+    var ordersServiceFactory = {};
+
+    var _getOrders = function () {
+
+        return $http.get(serviceBase + 'api/orders').then(function (results) {
+            return results;
+        });
+    };
+
+    ordersServiceFactory.getOrders = _getOrders;
+
+    return ordersServiceFactory;
+
+}]);
