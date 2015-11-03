259002ace6c7885d47666ef95d8a81eca6466832
jeffward01 <jeffwardcoding@gmail.com>
2015-11-03 15:01:35 -0800

    built layout and app.js

--- Authentication/Authentication.Web/js/app.js
+++ Authentication/Authentication.Web/js/app.js
@@ -1,1 +1,30 @@
-﻿
+﻿var app = angular.module('AngularAuthApp', ['ngRoute', 'LocalStorageModule', 'angular-loading-bar', 'ngResource', 'ui.router']);
+
+app.config(function ($routeProvider) {
+
+    $routeProvider.when("/home", {
+        controller: "homeController",
+        templateUrl: "/js/views/home.html"
+    });
+
+    $routeProvider.when("/login", {
+        controller: "loginController",
+        templateUrl: "/js/views/login.html"
+    });
+
+    $routeProvider.when("/signup", {
+        controller: "signupController",
+        templateUrl: "/js/views/signup.html"
+    });
+
+    $routeProvider.when("/orders", {
+        controller: "ordersController",
+        templateUrl: "/js/views/orders.html"
+    });
+
+    $routeProvider.otherwise({ redirectTo: "/home" });
+});
+
+app.run(['authService', function (authService) {
+    authService.fillAuthData();
+}]);
