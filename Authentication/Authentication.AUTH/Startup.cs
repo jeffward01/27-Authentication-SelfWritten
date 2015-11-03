4a8a4f016850c13bbf4587d69bb0c1fc0b649703
jeffward01 <jeffwardcoding@gmail.com>
2015-11-03 12:18:40 -0800

    added Owin

---
+++ Authentication/Authentication.AUTH/Startup.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Owin;
+using Microsoft.Owin;
+using System.Web;
+using System.Web.Http;
+
+[assembly: OwinStartup(typeof(Authentication.AUTH.API.Startup))]
+namespace Authentication.AUTH.API
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            HttpConfiguration config = new HttpConfiguration();
+            WebApiConfig.Register(config);
+            app.UseWebApi(config);
+        }
+    }
+}
