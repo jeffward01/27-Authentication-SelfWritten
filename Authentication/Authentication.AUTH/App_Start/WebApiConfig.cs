01a604035e2a94aa7661a977532538cfddc1965b
jeffward01 <jeffwardcoding@gmail.com>
2015-11-03 14:34:12 -0800

    building front end

--- Authentication/Authentication.AUTH/App_Start/WebApiConfig.cs
+++ Authentication/Authentication.AUTH/App_Start/WebApiConfig.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using Newtonsoft.Json.Serialization;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http.Formatting;
 using System.Web.Http;
 
 namespace Authentication.AUTH
@@ -13,12 +15,14 @@ namespace Authentication.AUTH
 
             // Web API routes
             config.MapHttpAttributeRoutes();
-
+            
             config.Routes.MapHttpRoute(
                 name: "DefaultApi",
                 routeTemplate: "api/{controller}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
+            var appXmlType = config.Formatters.XmlFormatter.SupportedMediaTypes.FirstOrDefault(t => t.MediaType == "application/xml");
+            config.Formatters.XmlFormatter.SupportedMediaTypes.Remove(appXmlType);
         }
     }
 }
